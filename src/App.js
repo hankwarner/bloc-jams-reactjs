e191e4f20b14ae570846a772b352bcc76110c381
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-12-30 18:24:39 -0500

    added material-ui nav bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 import albums from './data/albums';
-import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout">
-        <header className="mdl-layout__header">
-        <div className="mdl-layout-icon"></div>
-        <div className="mdl-layout__header-row">
-          <span className="mdl-layout__title">Bloc Jams</span>
-          <div className="mdl-layout-spacer"></div>
-          <nav className="mdl-navigation">
-            <Link to='/' className="mdl-navigation__link">Landing</Link>
-            <Link to='/library' className="mdl-navigation__link">Library</Link>
-          </nav>
-        </div>
+      <div>
+        <header>
+          <Header />
         </header>
-        <main className="mdl-layout__content">
+        <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
           <Route path="/album/:slug" component={Album} />
