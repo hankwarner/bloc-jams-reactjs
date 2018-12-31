51abad7f9fc0acb105cc29651c857519e588a336
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-12-30 23:11:47 -0500

    added cards to library

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,27 +1,58 @@
 import React from 'react';
-import '../index.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const Landing = () => (    
-    <section className="landing">
-        <h1 className="hero-title">Turn the music up!</h1>
-        <section>
-            <img src={'https://www.dropbox.com/s/xpb12gq6j5cnugu/headphones.jpg?raw=1'} alt='man wearing headphones' className="headphones-photo" />
-        </section>
-        <section className="selling-points">
-            <div className="point">
-                <h2 className="point-title">Choose your music</h2>
-                <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-            </div>
-            <div className="point">
-                <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-                <p className="point-description">No arbitrary limits. No distractions.</p>
-            </div>
-            <div className="point">
-                <h2 className="point-title">Mobile enabled</h2>
-                <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-            </div>
-        </section>
-    </section>
-);
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing.unit * 2,
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    }
+});
 
-export default Landing;
+function Landing(props) {
+    const { classes } = props;
+  
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+                <img src='https://www.dropbox.com/s/xpb12gq6j5cnugu/headphones.jpg?raw=1' alt='man wearing headphones' />
+                <h2>Turn the music up!</h2>
+            </Paper>
+            
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+                <h4>Choose your music</h4>
+                <p>The world is full of music; why should you have to listen to music that someone else chose?</p>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+                <h4>Unlimited, streaming, ad-free</h4>
+                <p>No arbitrary limits. No distractions</p>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+                <h4>Mobile enabled</h4>
+                <p>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+}
+  
+Landing.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+export default withStyles(styles)(Landing);
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,32 +1,90 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
-import '../index.css';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 345,
+    },
+        media: {
+        height: 140,
+    },
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing.unit * 2,
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    }
+});
 
 class Library extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { albums: albumData };
-    }
-    
+    state = { 
+        albums: albumData 
+    };
+  
     render() {
+        const { classes } = this.props;
+        
         return (
-            <div className="library-albums">
-                <section className='library'>
-                    { 
-                        this.state.albums.map( (album, index) =>
-                            <Link to={`/album/${album.slug}`} key={index}>
-                                <img src={album.albumCover} alt={album.title} className="album-photo" />
-                                <div className="album-block">{album.title}</div>
-                                <div className="album-block">{album.artist}</div>
-                                <div className="album-block-bottom">{album.songs.length} songs</div>
-                            </Link>
-                        )
-                    }
-                </section>
-            </div>
+        <div className={classes.root}>
+            <Grid container spacing={24}>
+                { 
+                    this.state.albums.map( (album, index) =>
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper}>
+                                <Card className={classes.card}>
+                                    <CardActionArea>
+                                        <CardMedia 
+                                            className={classes.media}
+                                            image={album.albumCover}
+                                            title={album.title}
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                <div>{album.title}</div>
+                                            </Typography>
+                                            <Typography component="p">
+                                                <div>{album.title}</div>
+                                                <div>{album.artist}</div>
+                                                <div>{album.songs.length} songs</div>
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button size="small" color="primary">
+                                            Listen
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                                
+                                <Link to={`/album/${album.slug}`} key={index}>
+                                </Link>
+
+                            </Paper>
+                        </Grid>
+                    )
+                }
+            </Grid>
+        </div>
         );
     }
 }
-
-export default Library;
+  
+Library.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+export default withStyles(styles)(Library);
