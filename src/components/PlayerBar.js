154e7675b0ed45fa98a42b0b57f8bcc10d59adc4
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-12-31 01:18:01 -0500

    adding mdl sliders to player bar

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,48 +1,52 @@
 import React, { Component } from 'react';
-import '../index.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
 
 class PlayerBar extends Component {
-
     render() {
+        const { classes } = this.props;
+
         return (
-            <section className="player-bar">
-                <section className="buttons">
-                    <button id="previous" className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect" onClick={this.props.handlePrevClick} >
+            <section>
+                <section>
+                    <button onClick={this.props.handlePrevClick} >
                         <span className="ion-skip-backward mdl-js-ripple-effect"></span>
                     </button>
-                    <button id="play-pause" className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" onClick={this.props.handleSongClick} >
+                    <button onClick={this.props.handleSongClick} >
                         <span className={this.props.isPlaying ? "ion-pause" : "ion-play" }></span>
                     </button>
-                    <button id="next" className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect" onClick={this.props.handleNextClick} >
+                    <button onClick={this.props.handleNextClick} >
                         <span className="ion-skip-forward"></span>
                     </button>
                 </section>
-                <div className="controls">
-                    <section className="time">
-                        <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
+                <div classes={{ container: classes.slider }}>
+                    <section>
+                        <div>{this.props.formatTime(this.props.currentTime)}</div>
                         <input 
-                            type="range" 
-                            className="track-slider"
+                            className="mdl-slider mdl-js-slider"
+                            type="range"
                             value={(this.props.currentTime / this.props.duration) || 0} 
                             max="1" 
                             min="0" 
                             step="0.01"
                             onChange={this.props.handleTimeChange}                        
                         />   
-                        <div className="total-time">{this.props.formatTime(this.props.duration)}</div> 
+                        <div>{this.props.formatTime(this.props.duration)}</div> 
                     </section>
-                    <section className="volume">
-                        <div id="volume-low" className="icon ion-volume-low"></div>
+                    <section>
+                        <div className="icon ion-volume-low"></div>
                         <input 
+                            className="mdl-slider mdl-js-slider"
                             type="range" 
-                            className="volume-slider"
                             value={this.props.currentVolume}
                             max="1.0" 
                             min="0.0"
                             step="0.01"
                             onChange={this.props.handleVolumeChange}
                         /> 
-                        <div id="volume-high" className="icon ion-volume-high"></div>
+                        <div className="icon ion-volume-high"></div>
                     </section>
                 </div>
             </section>
@@ -51,4 +55,8 @@ class PlayerBar extends Component {
     }
 }
 
-export default PlayerBar;
+PlayerBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+  export default withStyles(styles)(PlayerBar);
